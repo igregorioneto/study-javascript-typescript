ad1dff8692f0d09ced9a59ebfc556a54e440aa8d
Gregório Neto <igregoriodoneto@gmail.com>
2022-11-29 12:49:10 -0300

    Construindo Fabrica de Veículos

---
+++ factory-method/factories/car-factory.ts
@@ -0,0 +1,10 @@
+import { Vehicle } from "../vehicle/vehicle";
+import { VehicleFactory } from "./vehicle-factory";
+import { Car } from '../vehicle/car';
+
+export class CarFactory extends VehicleFactory {
+  getVehicle(vehicleName: string): Vehicle {
+    return new Car(vehicleName);
+  }
+
+}
