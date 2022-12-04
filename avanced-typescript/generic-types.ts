77a4c72b0c518f018e4bae50189e837d5b2d49ac
Gregório Neto <igregoriodoneto@gmail.com>
2022-12-04 00:33:30 -0300

    Genect Types

---
+++ avanced-typescript/generic-types.ts
@@ -0,0 +1,7 @@
+function save(args: T): T {
+  return args;
+}
+
+save({name: 'Daniel', age: 32});
+save({phone: '5551900000000', mail: 'mail@domain.com'});
+save({postalCode: '90000000', city: 'City Name'});
